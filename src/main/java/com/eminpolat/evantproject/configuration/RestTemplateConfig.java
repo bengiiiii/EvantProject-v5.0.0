78a7ac9ad5623c0073ba81f8d6fac144a4750b08
bengi <123201196+bengiiiii@users.noreply.github.com>
2025-08-19 15:43:43 +0300

    Update RestTemplateConfig.java

--- src/main/java/com/eminpolat/evantproject/configuration/RestTemplateConfig.java
+++ src/main/java/com/eminpolat/evantproject/configuration/RestTemplateConfig.java
@@ -7,7 +7,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder b) {
+        return b
+            .setConnectTimeout(Duration.ofSeconds(5))
+            .setReadTimeout(Duration.ofSeconds(5))
+            .build();
     }
 }
